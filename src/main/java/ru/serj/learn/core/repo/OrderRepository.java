c542e54f5f4ffded1fa93ae91c9288fac973f189
Sergey Teplov <serj.teplov>
2023-08-23 15:49:35 +0300

    part1

--- src/main/java/ru/serj/learn/core/repo/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.serj.learn.core.repo;
-
-import ru.serj.learn.core.model.Order;
-
-public interface OrderRepository {
-    void create(Order order);
-
-}
--- src/main/java/ru/serj/learn/core/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.serj.learn.core.repo;
-
-import ru.serj.learn.core.model.User1;
-
-import java.util.UUID;
-
-public interface UserRepository {
-    User1 find(UUID id);
-}
